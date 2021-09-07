1ce519fd8ae1278630f9fe61b0bdb3fc548a8ff4
Viet Le <lttviet@gmail.com>
2021-09-06 22:52:30 -0400

    Pretty login form (no logic yet)

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,19 +1,45 @@
+import { TextField, Typography, Grid, Button } from '@material-ui/core'
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Link from 'next/link'
 
-const Login: NextPage = () => {
+type LoginProps = {
+  loggedIn: boolean
+}
+
+const Login: NextPage<LoginProps> = ({ loggedIn }: LoginProps) => {
   return (
     <>
       <Head>
         <title>Login</title>
       </Head>
 
-      <h1>Login</h1>
-
-      <button>Login</button>
-
-      <button>Logout</button>
+      <Grid
+        container
+        spacing={3}
+        direction="column"
+        justifyContent="center"
+        alignItems="stretch"
+      >
+        <Grid item xs={12} sm={4}>
+          <Typography variant="h4">Sign in</Typography>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <TextField
+            label="Email"
+            fullWidth
+          />
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Button variant="contained" color="primary">
+            Email link
+          </Button>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="body1">
+            Firebase is used for passwordless authentication.
+          </Typography>
+        </Grid>
+      </Grid>
     </>
   )
 }
