978ddce8eaf6c0174d0ff9eab10e9e2a55d3dff6
Viet Le <lttviet@gmail.com>
2021-10-01 17:25:19 -0400

    Remove tested API

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,8 @@
-import { Link, List, ListItem, ListItemText, Typography } from '@mui/material'
+import { Typography } from '@mui/material'
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import { useEffect, useState } from 'react'
 
 const Home: NextPage = () => {
-  const [error, setError] = useState(null)
-  const [isLoaded, setIsLoaded] = useState(false)
-  const [articles, setArticles] = useState(
-    [{ id: 123, title: 'Test', url: 'none' }]
-  )
-
-  useEffect(() => {
-    fetch('https://api.spaceflightnewsapi.net/v3/articles')
-      .then(res => res.json())
-      .then(
-        (res) => {
-          setIsLoaded(true)
-          setError(null)
-          setArticles(res)
-        },
-        (error) => {
-          setIsLoaded(true)
-          setError(error)
-        })
-  }, [])
-
   return (
     <>
       <Head>
@@ -36,23 +14,6 @@ const Home: NextPage = () => {
         Enjoy your stay.
         Currently WIP.
       </Typography>
-
-      {!isLoaded &&
-        <Typography variant="h4">Loading...</Typography>
-      }
-
-      <List>
-        {
-          articles.map((article) => (
-            <ListItem key={article.id}>
-              <ListItemText
-                primary={article.title}
-                secondary={<Link href={article.url}>{article.url}</Link>}
-              />
-            </ListItem>
-          ))
-        }
-      </List>
     </>
   )
 }
