b4b513457b4b0a4536ec02121298432c991b9828
Viet Le <lttviet@gmail.com>
2021-10-15 10:52:31 -0400

    Remove getStaticProps

--- pages/stocks/[ticker].tsx
+++ pages/stocks/[ticker].tsx
@@ -1,39 +1,52 @@
 import { LinearProgress, Typography } from "@mui/material"
 import { ParentSize } from '@visx/responsive'
-import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType, NextPage } from "next"
+import { NextPage } from "next"
+import { useRouter } from "next/router"
 import Chart from "../../components/chart"
 import Layout from "../../components/layout"
 import useCandle from "../../hooks/useCandle"
 import { tickers } from "../../lib/tickers"
 
-export const getStaticProps: GetStaticProps = async (context) => {
-  return { props: { ticker: context.params?.ticker } }
-}
+const Stock: NextPage = () => {
+  const router = useRouter()
+  const ticker = router.query.ticker as string
+  const { data, loading, error } = useCandle(ticker as string)
 
-export const getStaticPaths: GetStaticPaths = async () => {
-  const paths = tickers.map(
-    (ticker) => ({ params: { ticker } })
-  )
+  if (!tickers.includes(ticker)) {
+    return (
+      <Layout>
+        <Typography variant="h4">
+          404 Page Not Found. TODO: custom 404 page.
+        </Typography>
+      </Layout>
+    )
+  }
 
-  return { paths, fallback: false }
-}
+  if (loading) {
+    return (
+      <Layout>
+        <LinearProgress />
+      </Layout>
+    )
+  }
 
-const Stock: NextPage = ({ ticker }: InferGetStaticPropsType<typeof getStaticProps>) => {
-  const { data, loading, error } = useCandle(ticker)
+  if (error) {
+    return (
+      <Layout>
+        <Typography variant="h4">Failed to get data</Typography>
+      </Layout>
+    )
+  }
 
   return (
     <Layout>
-      {loading && <LinearProgress />}
-      {error && <Typography variant="h4">Failed to get data</Typography>}
-      {!loading && !error &&
-        <div>
-          <ParentSize>
-            {({ width }) => (
-              <Chart data={data} width={width} height={400} />
-            )}
-          </ParentSize>
-        </div>
-      }
+      <div>
+        <ParentSize>
+          {({ width }) => (
+            <Chart data={data} width={width} height={400} />
+          )}
+        </ParentSize>
+      </div>
     </Layout>
   )
 }
